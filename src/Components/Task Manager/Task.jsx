2d67bbe27f34a15072ac45fd426e8ff6cf0a5c42
Altaf Hussain <altafsky17@gmail.com>
2023-05-07 18:47:43 +0530

    jsx and Partailly implementations

--- src/Components/Task Manager/Task.jsx
+++ src/Components/Task Manager/Task.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaEdit, FaTrashAlt, FaCheckDouble } from 'react-icons/fa'
 
-const Task = () => {
-  return (
-    <div>Task</div>
-  )
+const Task = (props) => {
+    const { id, name, date, complete } = props
+    return (
+        <div className={complete ? "task complete" : "task"} key={id}>
+
+            <span>
+                <p><b>Task:</b> {name}</p>
+                <p><b>Date:</b> {date}</p>
+            </span>
+
+            <span>
+                <button>
+                    <FaEdit color="green" />
+                </button>
+                <button>
+                    <FaTrashAlt color="red" />
+                </button>
+                <button>
+                    <FaCheckDouble color="green" />
+                </button>
+            </span>
+        </div>
+    )
 }
 
 export default Task
--- src/Components/Task Manager/TaskManager.jsx
+++ src/Components/Task Manager/TaskManager.jsx
@@ -1,10 +1,85 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './TaskManager.css'
+import Task from './Task'
 
 const TaskManager = () => {
-  return (
-    <div>TaskManager</div>
-  )
+    const [name, setName] = useState("");
+    const [date, setDate] = useState("");
+    const [tasks, setTasks] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!name && !date || !name || !date) {
+            alert("please enter task name & Date")
+        } else {
+            const newTask = {
+                id: Date.now(),
+                name,
+                date,
+                complete: true
+            }
+            setTasks([...tasks, newTask])
+            setName("")
+            setDate("")
+        }
+
+
+    }
+    const NameRef = useRef(null);
+
+    useEffect(() => {
+        NameRef.current.focus()
+    }, [])
+    return (
+        <div className='--bg-primary'>
+            <h1 className='--text-center --text-light'>Task Manager</h1>
+            <div className="--flex-center --p">
+                <div className="--card --bg-light --width-500px --p --flex-center">
+                    <form className='form --form-control' onSubmit={handleSubmit}>
+                        <div className="">
+                            <label htmlFor="name">Task:</label>
+                            <input
+                                type="text"
+                                placeholder='Task Name'
+                                name='name'
+                                value={name}
+                                ref={NameRef}
+                                onChange={(e) => setName(e.target.value)} />
+                        </div>
+                        <div className="">
+                            <label htmlFor="name">Task:</label>
+                            <input
+                                type="date"
+                                placeholder='Task Name'
+                                name='date'
+                                value={date}
+                                onChange={(e) => setDate(e.target.value)} />
+                        </div>
+                        <button type='submit' className="--btn --btn-success --btn-block">Save Task</button>
+                    </form>
+                </div>
+            </div>
+            <article className='--flex-center --my2'>
+                <div className="--width-500px --p">
+                    <h2 className='--text-light'>Task List</h2>
+                    <hr style={{ background: "#fff" }} />
+                    {tasks.length === 0 ? (
+                        <p className='--text-light'>No Task Added</p>
+                    ) : (
+                        <div>
+                            {tasks.map((item) => {
+                                return <Task
+                                    {...item}
+                                />
+                            })}
+                        </div>
+
+                    )}
+                </div>
+            </article>
+        </div>
+    )
 }
 
 export default TaskManager
