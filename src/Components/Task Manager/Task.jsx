ce57d1f69d505aa15656ff639f82711d4b5b8216
Altaf Hussain <altafsky17@gmail.com>
2023-05-10 13:47:41 +0530

    tasks Manager all part Completed

--- src/Components/Task Manager/Task.jsx
+++ src/Components/Task Manager/Task.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { FaEdit, FaTrashAlt, FaCheckDouble } from 'react-icons/fa'
 
 const Task = (props) => {
-    const { id, name, date, complete } = props
+    const { id, name, date, complete, editTask, deleteTask, completeTask } = props
     return (
         <div className={complete ? "task complete" : "task"} key={id}>
 
@@ -13,13 +13,13 @@ const Task = (props) => {
 
             <span>
                 <button>
-                    <FaEdit color="green" />
+                    <FaEdit color="green" onClick={() => editTask(id)} />
                 </button>
                 <button>
-                    <FaTrashAlt color="red" />
+                    <FaTrashAlt color="red" onClick={() => deleteTask(id)} />
                 </button>
                 <button>
-                    <FaCheckDouble color="green" />
+                    <FaCheckDouble color="purple" onClick={() => completeTask(id)} />
                 </button>
             </span>
         </div>
--- src/Components/Task Manager/TaskManager.jsx
+++ src/Components/Task Manager/TaskManager.jsx
@@ -1,23 +1,44 @@
-import React, { useState, useRef, useEffect } from 'react'
-import './TaskManager.css'
-import Task from './Task'
+import React, { useState, useRef, useEffect } from 'react';
+import './TaskManager.css';
+import Task from './Task';
+import useLocalStorage from 'use-local-storage';
 
 const TaskManager = () => {
     const [name, setName] = useState("");
     const [date, setDate] = useState("");
-    const [tasks, setTasks] = useState([]);
+    // const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useLocalStorage("tasks",[]);
+
+    const [taskId, setTaskId] = useState("");
+    const [isEditing, setIsEditing] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
         if (!name && !date || !name || !date) {
             alert("please enter task name & Date")
-        } else {
+        }
+        // edit functionality
+        else if (name && date && isEditing) {
+            setTasks(
+                tasks.map((task) => {
+                    if (task.id === taskId) {
+                        return { ...tasks, name, date, complete: false }
+                    }
+                    return task;
+                })
+            )
+            setName("")
+            setDate("")
+            setIsEditing(false)
+            setTaskId(null)
+        }
+        else {
             const newTask = {
                 id: Date.now(),
                 name,
                 date,
-                complete: true
+                complete: false
             }
             setTasks([...tasks, newTask])
             setName("")
@@ -31,6 +52,33 @@ const TaskManager = () => {
     useEffect(() => {
         NameRef.current.focus()
     }, [])
+    // edit functionality
+    const editTask = (id) => {
+        const thisTask = tasks.find((task) => task.id === id);
+        setIsEditing(true);
+        setTaskId(id);
+        setName(thisTask.name)
+        setDate(thisTask.date)
+    }
+    // delete functionality
+    const deleteTask = (id) => {
+        if (window.confirm("Delete This Task") === true) {
+            const newTask = tasks.filter((task) => task.id !== id)
+            setTasks(newTask)
+        }
+    }
+    // complete functionality
+    const completeTask = (id) => {
+        setTasks(
+            tasks.map((task) => {
+                if (task.id === id) {
+                    return { ...task, complete: true }
+                }
+                return task;
+            })
+        )
+    }
+
     return (
         <div className='--bg-primary'>
             <h1 className='--text-center --text-light'>Task Manager</h1>
@@ -48,7 +96,7 @@ const TaskManager = () => {
                                 onChange={(e) => setName(e.target.value)} />
                         </div>
                         <div className="">
-                            <label htmlFor="name">Task:</label>
+                            <label htmlFor="name">Date:</label>
                             <input
                                 type="date"
                                 placeholder='Task Name'
@@ -56,7 +104,7 @@ const TaskManager = () => {
                                 value={date}
                                 onChange={(e) => setDate(e.target.value)} />
                         </div>
-                        <button type='submit' className="--btn --btn-success --btn-block">Save Task</button>
+                        <button type='submit' className="--btn --btn-success --btn-block">{isEditing ? "Edit Task" : "Save Task"}</button>
                     </form>
                 </div>
             </div>
@@ -68,9 +116,12 @@ const TaskManager = () => {
                         <p className='--text-light'>No Task Added</p>
                     ) : (
                         <div>
-                            {tasks.map((item) => {
-                                return <Task
+                            {tasks.map((item ,ind) => {
+                                return <Task key={ind}
                                     {...item}
+                                    editTask={editTask}
+                                    deleteTask={deleteTask}
+                                    completeTask={completeTask}
                                 />
                             })}
                         </div>
